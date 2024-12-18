65db1940aa134d75c55b391ce7b0739cbbaea1f4
Székely <sz.david0202@gmail.com>
2024-12-18 13:20:54 +0100

    Add frontend project

---
+++ FoodDeliveryServer/src/main/java/com/fooddelivery/fooddeliveryserver/FoodDeliveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.fooddelivery.fooddeliveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryServerApplication.class, args);
+    }
+
+}
---
+++ FoodDeliveryServer/src/test/java/com/fooddelivery/fooddeliveryserver/FoodDeliveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fooddelivery.fooddeliveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
